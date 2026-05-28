1e7bb41296ef87970952607dda310976f54e5338
AlexSMFerreira <alexandre.smferreira@gmail.com>
2026-05-28 20:35:25 +0100

    hydra servo fix

--- HYDRA/src/Peripherals/Valves.cpp
+++ HYDRA/src/Peripherals/Valves.cpp
@@ -1,7 +1,11 @@
 #include "Peripherals/Valves.h"
 #include "Comms.h"
+#include "Servo.h"
 
-int valves_setup(data_t *data) {
+Servo servo;
+
+int valves_setup(data_t *data)
+{
     pinMode(SOL_VALVE_1_PIN, OUTPUT);
     pinMode(SOL_VALVE_2_PIN, OUTPUT);
     pinMode(SOL_VALVE_3_PIN, OUTPUT);
@@ -10,69 +14,75 @@ int valves_setup(data_t *data) {
     pinMode(ST_VALVE_1_PIN, OUTPUT);
     pinMode(ST_VALVE_2_PIN, OUTPUT);
     pinMode(CAM_EN_PIN, OUTPUT);
-    pinMode(PWM_SIG_PIN, OUTPUT);
-    
+
+    servo.attach(PWM_SIG_PIN, SERVO_OPEN_PULSE_WIDTH, SERVO_CLOSE_PULSE_WIDTH);
     close_all_valves(data);
     return 0;
 }
 
-void valve_set(data_t *data, uint8_t valve, uint8_t state) {
-    switch (valve) {
-        case VALVE_QUICK_DC_1:
-            digitalWrite(QDC_VALVE_1_PIN, state);
-            data->valve_states.v_quick_dc_1 = state;
-            break;
-        case VALVE_QUICK_DC_2:
-            digitalWrite(QDC_VALVE_2_PIN, state);
-            data->valve_states.v_quick_dc_2 = state;
-            break;
-        case VALVE_CONTROLLED_1:
-            if(DEFAULT_ID == HYDRA_LF_ID) {
+void valve_set(data_t *data, uint8_t valve, uint8_t state)
+{
+    switch (valve)
+    {
+    case VALVE_QUICK_DC_1:
+        digitalWrite(QDC_VALVE_1_PIN, state);
+        data->valve_states.v_quick_dc_1 = state;
+        break;
+    case VALVE_QUICK_DC_2:
+        digitalWrite(QDC_VALVE_2_PIN, state);
+        data->valve_states.v_quick_dc_2 = state;
+        break;
+    case VALVE_CONTROLLED_1:
+        if (DEFAULT_ID == HYDRA_LF_ID)
+        {
             // the abort valve is normally open, so invert the logic
             digitalWrite(SOL_VALVE_1_PIN, state == 0 ? 1 : 0);
             data->valve_states.v_controlled_1 = state;
-            } else {
+        }
+        else
+        {
             digitalWrite(SOL_VALVE_1_PIN, state);
             data->valve_states.v_controlled_1 = state;
-            }
-            break;
-        case VALVE_CONTROLLED_2:
-            digitalWrite(SOL_VALVE_2_PIN, state);
-            data->valve_states.v_controlled_2 = state;
-            break;
-        case VALVE_CONTROLLED_3:
-            digitalWrite(SOL_VALVE_3_PIN, state);
-            data->valve_states.v_controlled_3 = state;
-            break;
-        case VALVE_STEEL_BALL_1:
-            digitalWrite(ST_VALVE_1_PIN, state);
-            data->valve_states.v_steel_ball_1 = state;
-            break;
-        case VALVE_STEEL_BALL_2:
-            digitalWrite(ST_VALVE_2_PIN, state);
-            data->valve_states.v_steel_ball_2 = state;
-            break;
-        case VALVE_SERVO:
-            data->valve_states.v_servo = state;
-            digitalWrite(PWM_SIG_PIN, HIGH);
-            if(state) {
-                // open
-                delayMicroseconds(SERVO_OPEN_PULSE_WIDTH);
-            } else {
-                // close
-                delayMicroseconds(SERVO_CLOSE_PULSE_WIDTH);
-            }
-            digitalWrite(PWM_SIG_PIN, LOW);
-            delay(19);
-            break;
-        default:
-            // Invalid valve
-            break;
+        }
+        break;
+    case VALVE_CONTROLLED_2:
+        digitalWrite(SOL_VALVE_2_PIN, state);
+        data->valve_states.v_controlled_2 = state;
+        break;
+    case VALVE_CONTROLLED_3:
+        digitalWrite(SOL_VALVE_3_PIN, state);
+        data->valve_states.v_controlled_3 = state;
+        break;
+    case VALVE_STEEL_BALL_1:
+        digitalWrite(ST_VALVE_1_PIN, state);
+        data->valve_states.v_steel_ball_1 = state;
+        break;
+    case VALVE_STEEL_BALL_2:
+        digitalWrite(ST_VALVE_2_PIN, state);
+        data->valve_states.v_steel_ball_2 = state;
+        break;
+    case VALVE_SERVO:
+        data->valve_states.v_servo = state;
+        if (state)
+        {
+            servo.writeMicroseconds(SERVO_OPEN_PULSE_WIDTH);
+        }
+        else
+        {
+            servo.writeMicroseconds(SERVO_CLOSE_PULSE_WIDTH);
+        }
+        delay(15); 
+        break;
+    default:
+        // Invalid valve
+        break;
     }
 }
 
-void close_all_valves(data_t *data) {
-    for(int i = 0; i < hydra_valve_count; i++) {
+void close_all_valves(data_t *data)
+{
+    for (int i = 0; i < hydra_valve_count; i++)
+    {
         valve_set(data, i, 0);
     }
 }
